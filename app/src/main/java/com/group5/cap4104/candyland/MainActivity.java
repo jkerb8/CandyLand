3a881d6ee0800171e0cc1238e549ffdd3e42809c
Eric Downey <edowney29@gmail.com>
2017-04-05 20:07:39 -0400

    add the test turns for the video

--- app/src/main/java/com/group5/cap4104/candyland/MainActivity.java
+++ app/src/main/java/com/group5/cap4104/candyland/MainActivity.java
@@ -1,7 +1,6 @@
 package com.group5.cap4104.candyland;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
